8d057b8d4a2f410abe3c50df16971d21c5d72264
hoomandaws <127763074+hoomandaws@users.noreply.github.com>
2023-08-28 11:56:59 +0000

    container config done

---
+++ Basic-one/variable.js
@@ -0,0 +1,9 @@
+//Always declare a variable with const when you know that the value should not be changed
+//const variables can neither be updated nor re-declared.
+const employeeID=9841;
+console.log(employeeID);
+
+// let variables can be updated but not re-declared.
+let studentID="196-sec1";
+studentID='sec-1';
+console.log(studentID);
