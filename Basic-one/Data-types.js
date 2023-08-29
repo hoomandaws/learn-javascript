84f21ebeb64b7f945cb5a6a642ea24b43213c544
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-08-29 17:15:48 +0000

    datatype

---
+++ Basic-one/Data-types.js
@@ -0,0 +1,25 @@
+/* All JavaScript numbers are stored as decimal numbers (floating point).
+Numbers can be written with, or without decimals:
+The Number type is a double-precision 64-bit binary format IEEE 754 value. */
+let a=55
+let b=5.5
+
+/*
+A string  is a series of characters like "Ram sita",'hanuman'.
+Strings are written with quotes. You can use single or double quotes:
+ */
+let one="ram"
+let two='hanuman'
+
+/**
+ * Booleans can only have two values: true or false.
+ */
+let nameId=5
+let teacherId=6
+console.log(nameId==teacherId);
+
+//undefined
+let maheshDaale;
+
+//null
+let employeSalary=null
