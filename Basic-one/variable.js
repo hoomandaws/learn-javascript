f99b7a809787f1ea28d647f77d823a258c1cfc73
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-08-29 16:38:56 +0000

    contener config done

--- Basic-one/variable.js
+++ Basic-one/variable.js
@@ -1,5 +1,5 @@
-//Always declare a variable with const when you know that the value should not be changed
-//const variables can neither be updated nor re-declared.
+/*Always declare a variable with const when you know that the value should not be changed
+const variables can neither be updated nor re-declared.*/
 const employeeID=9841;
 console.log(employeeID);
 
@@ -7,7 +7,11 @@ console.log(employeeID);
 let studentID="196-sec1";
 studentID='sec-1';
 console.log(studentID);
-//var 
+/*
+ They do not support block-level scope. This means that if a variable is defined in a loop or in an 
+ if statement it can be accessed outside the block and accidentally redefined leading to a buggy program.
+*/
 var collegeId="72har";
 collegeId="98ram";
+console.log(collegeId);
 console.table([employeeID,studentID,collegeId])
