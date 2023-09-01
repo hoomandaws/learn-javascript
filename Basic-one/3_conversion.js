f05230d267d78d24f4494da8582377e6174d4f4f
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-09-01 14:11:54 +0000

    conversion done

---
+++ Basic-one/3_conversion.js
@@ -0,0 +1,13 @@
+// Number conversion in datatypes
+let coders="anuj"
+let coderID=Number(coders)
+console.log(coderID);
+
+//boolean conversion
+let booleanID=1
+let booleanone=0
+let covbooleanId=Boolean(booleanID)
+let covbooleanone=Boolean(booleanone)
+console.log(covbooleanId,covbooleanone);
+
+//
