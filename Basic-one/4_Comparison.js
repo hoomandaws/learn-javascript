c3fa0a81f67fde666cccae8dbbbfa55a274b2fdb
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-09-01 16:55:45 +0000

    comparision done

---
+++ Basic-one/4_Comparison.js
@@ -0,0 +1,19 @@
+//Simple comparison
+let a=5
+let b=3
+console.log(a==b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a!=b);
+
+//null comparision
+console.log(null==b);
+console.log(null>a);
+
+//undefined comparision
+console.log(undefined==a);
+console.log(undefined!=b);
+
+//Strict equality check or triple check
+console.log("5"===a);
+console.log("r"==="r");
