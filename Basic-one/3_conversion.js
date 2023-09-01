484cde7e1714fe09e97702f096a04ab5c2a2facb
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-09-01 14:20:55 +0000

    conversion done

--- Basic-one/3_conversion.js
+++ Basic-one/3_conversion.js
@@ -1,13 +1,20 @@
-// Number conversion in datatypes
+// Number conversion 
+// string into number conversion
 let coders="anuj"
 let coderID=Number(coders)
-console.log(coderID);
+console.log( typeof coderID);
 
 //boolean conversion
+// number in boolean conversion
 let booleanID=1
 let booleanone=0
 let covbooleanId=Boolean(booleanID)
 let covbooleanone=Boolean(booleanone)
 console.log(covbooleanId,covbooleanone);
 
-//
+//string conversion
+//number in string conversion
+let num = 55
+let numone=String(num)
+console.log(numone);
+console.log(typeof (numone));
