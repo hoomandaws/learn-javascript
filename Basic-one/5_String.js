4bc6f1f2a20cde973a91e4440ef67b0ca9c46dda
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-09-06 16:27:25 +0000

    string config done

---
+++ Basic-one/5_String.js
@@ -0,0 +1,10 @@
+/**STRING:-
+ *JavaScript String Object is a sequence of characters. 
+ *It contains zero or more characters within single or double quotes."Anuj",'Anuj'
+ */
+//length
+let strOne=new String("Anuj Dhungana")
+console.log(strOne.length);
+//uppercase and lowercase function in string
+let strTwo=new String("KaDaghari")
+console.log(strTwo.toUpperCase());
