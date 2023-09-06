fc0ff3a9836eb82e060df5a11695a64a28af90e9
Anuj Dhungana <127763074+hoomandaws@users.noreply.github.com>
2023-09-06 16:55:02 +0000

    String config Done

--- Basic-one/5_String.js
+++ Basic-one/5_String.js
@@ -2,9 +2,21 @@
  *JavaScript String Object is a sequence of characters. 
  *It contains zero or more characters within single or double quotes."Anuj",'Anuj'
  */
+//String Concatinate
+let name="Amita Dhungana"
+let Address="kadaghari"
+console.log( `Hello,I am ${name} I live in ${Address}` );
+
+//string as object
 //length
 let strOne=new String("Anuj Dhungana")
 console.log(strOne.length);
 //uppercase and lowercase function in string
 let strTwo=new String("KaDaghari")
 console.log(strTwo.toUpperCase());
+console.log(strTwo.toLowerCase());
+
+/**
+ * There are lots of String of methods 
+ * some are above and other you can go through javascript MDN site. 
+ */
